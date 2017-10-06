bf79e2f892dffe8b79794bb4bf87fa95fc018de1
kostmita14 <przeor14@gmail.com>
2017-10-06 19:38:54 +0200

    persistent volume added

--- rabbitmq/start.sh
+++ rabbitmq/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-docker run -d --net=host -p 15672:15672 -p 5672:5672 --name some-rabbit --hostname localhost myrabbitmq
+docker run -d --net=host -p 15672:15672 -p 5672:5672 --name some-rabbit --hostname localhost -v /media/pi/ext250/rabbitmq_data:/var/lib/rabbitmq myrabbitmq
