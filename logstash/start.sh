27c86f95c0b6b790742d674583216595f3f58299
kostmita14 <przeor14@gmail.com>
2017-10-08 16:55:46 +0200

    usage of two different images to compare resource consumption

--- logstash/start.sh
+++ logstash/start.sh
@@ -3,5 +3,5 @@ set -e
 
 #docker run -d -p 9600:9600 --name some-logstash --link some-rabbit --link some-elasticsearch:localhost mylogstash
 #docker run -d --net=host -p 9600:9600 --name some-logstash mylogstash
-docker-compose up -d --scale some-logstash=2
+docker-compose up -d --scale logstash=2 --scale logstash_alpine=2 --remove-orphans
 
