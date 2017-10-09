7feb1014d62d14bd6023d0fd2a0a7e6e2c93eb1f
kostmita14 <przeor14@gmail.com>
2017-10-09 18:12:38 +0200

    switch to docker-compose

--- netdata/start.sh
+++ netdata/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-#docker run --rm -d --name some-logstash --link some-rabbit --link myelastic:localhost mylogstash
-#docker run -d --rm --name some-elasticsearch -v /media/pi/ext250/elastic_data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 elasticsearch
+#docker run -d -p 9600:9600 --name some-logstash --link some-rabbit --link some-elasticsearch:localhost mylogstash
+#docker run -d --net=host -p 9600:9600 --name some-logstash mylogstash
+docker-compose up -d --scale logstash=2 --scale logstash_alpine=2 --remove-orphans
 
-docker run -d --name some-netdata --net=host --cap-add SYS_PTRACE -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /var/run/docker.sock:/var/run/docker.sock -p 19999:19999 --privileged titpetric/netdata
