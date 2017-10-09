b0b5a7817e309970395441a8d888ea618603674a
kostmita14 <przeor14@gmail.com>
2017-10-09 19:38:07 +0200

    switch to docker-compose

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-docker run -d --name some-elasticsearch -v /media/pi/ext250/elastic_data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 elasticsearch
+docker-compose up -d --remove-orphans
+
