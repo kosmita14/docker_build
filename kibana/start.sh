d221b09f246a6b213ed42e128d9154050124d90b
kostmita14 <przeor14@gmail.com>
2017-10-15 09:11:47 +0200

    Switch to docker-composer

--- kibana/start.sh
+++ kibana/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-#docker run -p 5601:5601 --link elasticsearch_elasticsearch_1:myelastic-url -e ELASTICSEARCH_URL=http://myelastic-url:9200 --name some-kibana -d kibana
-docker run -p 5601:5601 --net host -e ELASTICSEARCH_URL=http://localhost:9200 --name some-kibana -d kibana
+docker-compose up -d --remove-orphans
+
--- kibana/stop.sh
+++ kibana/stop.sh
@@ -1,46 +1,47 @@
 #!/bin/sh
 set -e
 
+docker-compose down
 
-running_containers="$(docker ps --filter "name=some-kibana" --format "{{.ID}}")"
+#running_containers="$(docker ps --filter "name=some-logstash" --format "{{.ID}}")"
 
-if [ "$running_containers" != "" ] 
-then
-    docker stop "$running_containers"
-else
-    echo "All good - no running containers !"
-fi
-
-
-all_containers="$(docker ps -a --filter "name=some-kibana" --format "{{.ID}}")"
-
-if [ "$all_containers" != "" ] 
-then
-    docker rm "$all_containers"
-else
-    echo "All good - no containers !"
-fi
+#if [ "$running_containers" != "" ] 
+#then
+#    docker stop "$running_containers"
+#else
+#    echo "All good - no running containers !"
+#fi
 
 
+#all_containers="$(docker ps -a --filter "name=some-logstash" --format "{{.ID}}")"
+#
+#if [ "$all_containers" != "" ] 
+#then
+#    docker rm "$all_containers"
+#else
+#    echo "All good - no containers !"
+#fi
 
 
 
-running_containers="$(docker ps --filter "ancestor=kibana" --format "{{.ID}}")"
-
-if [ "$running_containers" != "" ] 
-then
-    docker stop "$running_containers"
-else
-    echo "All good - no running containers !"
-fi
 
 
-all_containers="$(docker ps -a --filter "ancestor=kibana" --format "{{.ID}}")"
+#running_containers="$(docker ps --filter "ancestor=mylogstash" --format "{{.ID}}")"
 
-if [ "$all_containers" != "" ] 
-then
-    docker rm "$all_containers"
-else
-    echo "All good - no containers !"
-fi
+#if [ "$running_containers" != "" ] 
+#then
+#    docker stop "$running_containers"
+#else
+#    echo "All good - no running containers !"
+#fi
+
+
+#all_containers="$(docker ps -a --filter "ancestor=mylogstash" --format "{{.ID}}")"
+
+#if [ "$all_containers" != "" ] 
+#then
+#    docker rm "$all_containers"
+#else
+#    echo "All good - no containers !"
+#fi
 
