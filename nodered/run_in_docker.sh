3679d7088c0ce80a5162276d757902bee9a8080b
kostmita14 <przeor14@gmail.com>
2017-11-04 09:08:59 +0100

    add docker-compose

---
+++ nodered/run_in_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run_in_docker.sh
+
+#chmod a+w /data/*.json
+npm install /data
+npm start -- --userDir /data
+
--- nodered/start.sh
+++ nodered/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-#docker run --rm -d -p 1880:1880 --name some-nodered -v /media/pi/ext250/nodered_data:/data mynodered
-docker run -d -p 1880:1880 --name some-nodered -v /media/pi/ext250/nodered_data:/data mynodered
+docker-compose up -d --remove-orphans
+
