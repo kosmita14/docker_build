8d422d4b08c0568794f785bb6abf198a15876b1b
kostmita14 <przeor14@gmail.com>
2017-11-18 19:50:27 +0100

    changes in backend section

--- netdata/start.sh
+++ netdata/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
-
-#docker run -d -p 9600:9600 --name some-logstash --link some-rabbit --link some-elasticsearch:localhost mylogstash
-#docker run -d --net=host -p 9600:9600 --name some-logstash mylogstash
-docker-compose up -d --scale logstash=2 --scale logstash_alpine=2 --remove-orphans
-
+LOGGING_DRIVER=fluentd
+export LOGGING_DRIVER
+docker-compose up -d 
