c044e1b9e1eed28e162e60bf7bf2584f85dfcfaa
kostmita14 <przeor14@gmail.com>
2018-03-04 21:17:42 +0100

    Merge of remaining logstash configs and move fluentd to elasticsearch

--- log_tests/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-LOGGING_DRIVER=fluentd
-export LOGGING_DRIVER
-docker-compose up -d 
--- metricbeat/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-docker-compose up --build beat
-
-#docker-compose up -d --remove-orphans
-
